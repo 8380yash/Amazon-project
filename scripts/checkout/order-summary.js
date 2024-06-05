19c7dac08e6a90a95dac629b911afcc5397b59dd
ErenJeager <ywani774@gmail.com>
2024-06-05 19:29:08 +0530

    save the data and generate the html for the payment summary.

--- scripts/checkout/order-summary.js
+++ scripts/checkout/order-summary.js
@@ -1,32 +1,39 @@
 import { cart, removeFromCart, UpdateQuantity, updateDeliveryOption } from "../../data/cart.js";
-import { products } from "../../data/products.js";
+import { products, getProduct } from "../../data/products.js";
 import { formatCurrency } from "./../utils/money.js";
 import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js";
-import { deliveryOptions } from "../../data/deliveryOption.js";
+import { deliveryOptions, getDeliveryOption } from "../../data/deliveryOption.js";
 
 export function renderOrderSummary() {
     let cartSummaryHTML = '';
 
     cart.forEach((cartItem) => {
         const productId = cartItem.productId;
-        let matchingProduct;
 
-        products.forEach((product) => {
-            if (product.id === productId) {
-                matchingProduct = product;
-            }
-        });
+      const matchingProduct = getProduct(productId);
+                  //or
+        // let matchingProduct;
+
+        // products.forEach((product) => {
+        //     if (product.id === productId) {
+        //         matchingProduct = product;
+        //     }
+        // });
 
 
         const deliveryOptionId = cartItem.deliveryOptionId;
 
-        let deliveryOption;
+        // let deliveryOption;
 
-        deliveryOptions.forEach((option) => {
-            if (option.id === deliveryOptionId) {
-                deliveryOption = option;
-            }
-        });
+        // deliveryOptions.forEach((option) => {
+        //     if (option.id === deliveryOptionId) {
+        //         deliveryOption = option;
+        //     }
+        // });
+
+                //or
+
+                const deliveryOption = getDeliveryOption(deliveryOptionId);
 
         const today = dayjs();
         const deliveryDate = today.add(deliveryOption.deliveryDays, 'days');
