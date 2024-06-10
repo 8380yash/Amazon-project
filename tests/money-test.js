55ec2ab7602248ec2e099bcded6b08173f7b9121
ErenJeager <ywani774@gmail.com>
2024-06-10 15:22:41 +0530

    create the automated test for the format currency.

---
+++ tests/money-test.js
@@ -0,0 +1,32 @@
+import { formatCurrency } from "../scripts/utils/money.js";
+
+console.log('testSuite:Format currency')
+//this is the basic test case
+console.log('converts the cents into the dollars')
+if(formatCurrency(2095) === '20.95'){
+    console.log('passed')
+} else {
+    console.log('failed');
+}
+
+
+//these two are the edge cases
+console.log('works with zero')
+if(formatCurrency(0) === '0.00'){
+    console.log('passed')
+} else {
+    console.log('failed');
+}
+
+console.log('Rounds up to the nearest cents')
+if(formatCurrency(2000.5) === '20.05'){
+    console.log('passed')
+} else {
+    console.log('failed');
+}
+
+if(formatCurrency(2000.4) === '20.00'){
+    console.log('passed')
+} else {
+    console.log('failed');
+}
