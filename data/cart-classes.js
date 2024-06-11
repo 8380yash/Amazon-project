d5fc44cc4cca115ac9cb3a6102c71613ffe6af0b
ErenJeager <ywani774@gmail.com>
2024-06-11 16:57:51 +0530

    create the class version of the class.

---
+++ data/cart-classes.js
@@ -0,0 +1,127 @@
+class Cart {
+    CartItems = undefined;
+    loadFromStorageKey = undefined;
+
+    constructor (loadFromStorageKey) {
+            this.loadFromStorageKey = loadFromStorageKey;
+            this.loadFromStorage();
+    }
+
+    loadFromStorage () {
+        this.CartItems = JSON.parse(localStorage.getItem(this.localStorageKey));
+      
+          if(!this.CartItems){
+             this.CartItems = [{
+                  productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+                  quantity:1,
+                  deliveryOptionId:'1'
+              },{
+                  productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
+                  quantity:2,
+                  deliveryOptionId:'2'
+              }];
+          }
+          
+      };
+
+      saveToStorage () {
+        localStorage.setItem(this.localStorageKey, JSON.stringify(this.CartItems));
+    };
+
+    addToCart (productId) {
+    
+        //this will check if the same product is in the cart
+        let matchingItem;
+        this.CartItems.forEach((cartItem) => {
+            if (productId === cartItem.productId) {
+                matchingItem = cartItem;
+            }
+        });
+    
+         //this code is for the select dropdown menu
+        //  const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`);
+    
+        //  const quantity = Number(quantitySelector.value);
+    
+        //if the same product in the cart we just update its quantity here
+        if (matchingItem) {
+            matchingItem.quantity += 1;
+        } else {
+            //if its not in the cart then we will push the product in the cart
+            this.CartItems.push({
+                productId,
+                quantity :1,
+                deliveryOptionId:'1'
+                //or
+                // productId: productId,
+                // quantity:quantity
+            });
+        }
+       this.saveToStorage();
+    };
+    removeFromCart (productId) {
+    
+        let newCart = [];
+        
+        this.CartItems.forEach((cartItem) => {
+            if(cartItem.productId != productId){
+                newCart.push(cartItem);
+            }
+        });
+        
+        this.CartItems = newCart;
+       this.saveToStorage();
+        };
+        
+        
+         
+        
+        UpdateQuantity(productId, newQuantity){
+    
+            let matchingItem;
+        
+            this.CartItems.forEach((cartItem) => {
+                    if(productId === cartItem.productId){
+                       matchingItem = cartItem
+                    }
+            });
+            matchingItem.quantity = newQuantity;
+            this.saveToStorage();
+        };
+
+        updateDeliveryOption (productId, deliveryOptionId) {
+            let matchingItem;
+            this.CartItems.forEach((cartItem) => {
+                if (productId === cartItem.productId) {
+                    matchingItem = cartItem;
+                }
+            });
+        
+            matchingItem.deliveryOptionId = deliveryOptionId;
+        
+           this.saveToStorage();
+        };
+
+}
+
+
+const cart = new Cart('cart-oop');
+const businessCart = new Cart('cart-business');
+
+
+
+    console.log(cart);
+    console.log(businessCart);
+    console.log(businessCart instanceof Cart);
+ 
+
+
+
+
+
+
+
+
+
+
+
