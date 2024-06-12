4c451b14e79d456d60df40f32d64a4a40943d3d8
ErenJeager <ywani774@gmail.com>
2024-06-12 12:45:22 +0530

    practicing the opps concept for the second time an create the class version of the cart for the seocnd time.

--- data/cart-classes.js
+++ data/cart-classes.js
@@ -1,98 +1,96 @@
 class Cart {
-    // CartItems = undefined;
-    CartItems;
-    #localStorageKey;
+    cartItems = undefined;
+    localStorageKey = undefined;
 
-    constructor (localStorageKey) {
-            this.#localStorageKey = localStorageKey;
-            this.#loadFromStorage();
+    constructor (localStorageKey)  {
+        this.localStorageKey = localStorageKey;
+        this.loadFromStorage(); 
     }
-
-    #loadFromStorage () {
-        this.CartItems = JSON.parse(localStorage.getItem(this.#localStorageKey));
-      
-          if(!this.CartItems){
-             this.CartItems = [{
-                  productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-                  quantity:1,
-                  deliveryOptionId:'1'
-              },{
-                  productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
-                  quantity:2,
-                  deliveryOptionId:'2'
-              }];
-          }
-          
-      };
-
-      saveToStorage () {
-        localStorage.setItem(this.#localStorageKey, JSON.stringify(this.CartItems));
-    };
-
-    addToCart (productId) {
-    
-        //this will check if the same product is in the cart
-        let matchingItem;
-        this.CartItems.forEach((cartItem) => {
-            if (productId === cartItem.productId) {
-                matchingItem = cartItem;
-            }
-        });
-    
-         //this code is for the select dropdown menu
-        //  const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`);
-    
-        //  const quantity = Number(quantitySelector.value);
-    
-        //if the same product in the cart we just update its quantity here
-        if (matchingItem) {
-            matchingItem.quantity += 1;
-        } else {
-            //if its not in the cart then we will push the product in the cart
-            this.CartItems.push({
-                productId,
-                quantity :1,
-                deliveryOptionId:'1'
-                //or
-                // productId: productId,
-                // quantity:quantity
-            });
-        }
-       this.saveToStorage();
-    };
-    removeFromCart (productId) {
-    
-        let newCart = [];
+    // loadFromStorage: function () {
+        loadFromStorage() {
+            this.cartItems = JSON.parse(localStorage.getItem(this.localStorageKey));
         
-        this.CartItems.forEach((cartItem) => {
-            if(cartItem.productId != productId){
-                newCart.push(cartItem);
+            if (!this.cartItems) {
+                this.cartItems = [{
+                    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+                    quantity: 1,
+                    deliveryOptionId: '1'
+                }, {
+                    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+                    quantity: 2,
+                    deliveryOptionId: '2'
+                }];
             }
-        });
         
-        this.CartItems = newCart;
-       this.saveToStorage();
         };
+
+        saveToStorage() {
+            localStorage.setItem(this.localStorageKey, JSON.stringify(this.cartItems));
+        };
+
+        addToCart(productId) {
+    
+            //this will check if the same product is in the cart
+            let matchingItem;
+            this.cartItems.forEach((cartItem) => {
+                if (productId === cartItem.productId) {
+                    matchingItem = cartItem;
+                }
+            });
         
+            //this code is for the select dropdown menu
+            // const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`);
         
-         
+            // const quantity = Number(quantitySelector.value);
         
-        UpdateQuantity(productId, newQuantity){
+            //if the same product in the cart we just update its quantity here
+            if (matchingItem) {
+                matchingItem.quantity += 1;
+            } else {
+                //if its not in the cart then we will push the product in the cart
+                this.cartItems.push({
+                    productId,
+                    quantity:1,
+                    deliveryOptionId: '1'
+                    //or
+                    // productId: productId,
+                    // quantity:quantity
+                });
+            }
+            this.saveToStorage();
+        };
+
+        
+        removeFromCart(productId) {
+    
+            let newCart = [];
+        
+            this.cartItems.forEach((cartItem) => {
+                if (cartItem.productId != productId) {
+                    newCart.push(cartItem);
+                }
+            });
+        
+            this.cartItems = newCart;
+           this.saveToStorage();
+        };
+    
+         UpdateQuantity(productId, newQuantity) {
     
             let matchingItem;
         
-            this.CartItems.forEach((cartItem) => {
-                    if(productId === cartItem.productId){
-                       matchingItem = cartItem
-                    }
+            this.cartItems.forEach((cartItem) => {
+                if (productId === cartItem.productId) {
+                    matchingItem = cartItem
+                }
             });
             matchingItem.quantity = newQuantity;
             this.saveToStorage();
         };
-
-        updateDeliveryOption (productId, deliveryOptionId) {
+        
+         updateDeliveryOption(productId, deliveryOptionId) {
             let matchingItem;
-            this.CartItems.forEach((cartItem) => {
+            this.cartItems.forEach((cartItem) => {
                 if (productId === cartItem.productId) {
                     matchingItem = cartItem;
                 }
@@ -100,23 +98,23 @@ class Cart {
         
             matchingItem.deliveryOptionId = deliveryOptionId;
         
-           this.saveToStorage();
-        };
-
+            saveToStorage();
+        }
+      
 }
 
 
-const cart = new Cart('cart-oop');
-const businessCart = new Cart('cart-business');
 
+        const cart = new Cart ('cart-oop');
+        const businessCart = new Cart ('cart-business');
 
+       
 
+        console.log(cart);
+        console.log(businessCart);
+        
+       
 
-
-    console.log(cart);
-    console.log(businessCart);
-    console.log(businessCart instanceof Cart);
- 
 
 
 
